e52bf4f9619e9a623a1eeccedf2790d79ed1de30
Adam Kuhns <abk03310@vsc.edu>
2022-12-11 20:28:11 -0500

    day3

---
+++ Day3/Part1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/adamkuhns/Desktop/Advent of Code-2022/Day3/input.txt");
+            string[] rucksack = input.Split('\n');
+            int total = 0;
+            
+
+            foreach (string x in rucksack)
+            {
+
+                char[] found = new char[104];
+                int foundCn = 0;
+                string compartment1 = x.Substring(0,(x.Length) / 2);
+                string compartment2 = x.Substring((x.Length)/2);
+
+                char[] cp1Arr = compartment1.ToCharArray();
+                
+                for (int i = 0; i < cp1Arr.Length; i++)
+                {
+                    
+                    
+                    if (compartment2.Contains(cp1Arr[i]))
+                    {
+                      bool exists = false;
+                      for(int inner = 0; inner < foundCn; inner++)
+                        {
+                            if (found[inner] == cp1Arr[i])
+                            {
+                                exists = true;
+                                break;
+                            }
+                        }
+                      if(exists == false)
+                        {
+                            found[foundCn] = cp1Arr[i];
+                            Console.Write(cp1Arr[i]+": ");
+                            if (Char.IsUpper(cp1Arr[i]))
+                            {
+                                total = total + (((int)cp1Arr[i]) - 38);
+                                Console.Write(((int)cp1Arr[i]) - 38);
+                            }
+                            else
+                            {
+                                total = total + (((int)cp1Arr[i]) - 96);
+                                Console.Write(((int)cp1Arr[i]) - 96);
+                            }
+                            foundCn++;
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                
+                //Console.WriteLine(compartment1 +" : "+ compartment2);
+            }
+            Console.WriteLine(total);
+            //Console.WriteLine(foundCn);
+        }
+    }
+}
---
+++ Day3/Part2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+//1007 to low
+//3040 to high
+namespace Day3P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/adamkuhns/Desktop/Advent of Code-2022/Day3/input.txt");
+            string[] rucksack = input.Split('\n');
+            int total = 0;
+
+
+            for (int i = 0; i < (rucksack.Length)-2; i++)
+            {
+                char[] cp1Arr = rucksack[i].ToCharArray();
+
+                for (int y = 0; y < cp1Arr.Length; y++)
+                {
+                    if (rucksack[i + 1].Contains(cp1Arr[y]) && rucksack[i + 2].Contains(cp1Arr[y]))
+                    {
+                        i = i + 2;
+                        if (Char.IsUpper(cp1Arr[y]))
+                        {
+                            total = total + (((int)cp1Arr[y]) - 38);
+                            Console.Write(((int)cp1Arr[y]) - 38);
+                        }
+                        else
+                        {
+                            total = total + (((int)cp1Arr[y]) - 96);
+                            Console.Write(((int)cp1Arr[y]) - 96);
+                        }
+                        Console.WriteLine();
+                        //Console.WriteLine(cp1Arr[y]);
+                        break;
+                    }
+                }
+
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
