b1e328147091ea8d4f390b759158de5b8f5cff9a
Adam Kuhns <abk03310@vsc.edu>
2022-12-19 19:18:46 -0500

    Day6

---
+++ Day6/Part1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/adamkuhns/Desktop/Advent of Code-2022/Day6/input.txt");
+            char[] lineChar = input.ToCharArray();
+            for (int i =0; i < lineChar.Length - 3; i++)
+            {
+                char[] check = {lineChar[i], lineChar[i+1], lineChar[i+2], lineChar[i+3] };
+                //Console.WriteLine(check);
+                bool uniq = true;
+
+                if (check.Count() != check.Distinct().Count())
+                {
+                    uniq = false;
+                }
+
+                if (uniq)
+                {
+                    Console.WriteLine(check);
+                    Console.WriteLine(i+4);
+                    break;
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ Day6/Part2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/adamkuhns/Desktop/Advent of Code-2022/Day6/input.txt");
+            char[] lineChar = input.ToCharArray();
+            for (int i = 0; i < lineChar.Length - 13; i++)
+            {
+                char[] check = new char[14];
+                for (int y = 0; y < 14; y++)
+                {
+                    check[y] = lineChar[i + y];
+                }
+
+                //Console.WriteLine(check);
+                bool uniq = true;
+
+                if (check.Count() != check.Distinct().Count())
+                {
+                    uniq = false;
+                }
+
+                if (uniq)
+                {
+                    Console.WriteLine(check);
+                    Console.WriteLine(i + 14);
+                    break;
+                }
+            }
+        }
+    }
+}
